f9bc7185eb2d57ec111d21e66c0435bed911a544
Thibaut Lienart <ltib@me.com>
2019-10-24 13:28:08 +0100

    Updates

--- scripts/A-learning-networks.jl
+++ scripts/A-learning-networks.jl
@@ -1,3 +1,12 @@
+# Before running this, please make sure to activate and instantiate the environment
+# corresponding to [this `Project.toml`](https://raw.githubusercontent.com/alan-turing-institute/MLJTutorials/master/Project.toml) and [this `Manifest.toml`](https://raw.githubusercontent.com/alan-turing-institute/MLJTutorials/master/Manifest.toml)
+# so that you get an environment which matches the one used to generate the tutorials:
+#
+# ```julia
+# cd("MLJTutorials") # cd to folder with the *.toml
+# using Pkg; Pkg.activate("."); Pkg.instantiate()
+# ```
+
 using MLJ, DataFrames, Random
 @load RidgeRegressor pkg=MultivariateStats
 
