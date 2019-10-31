85cc7d32375a79bdcca05be92e1749332a1e7dc3
Thibaut Lienart <ltib@me.com>
2019-10-31 14:58:40 +0100

    no tests for plotting tutorials

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,13 @@ curdir = @__DIR__
 scripts_dir = normpath(joinpath(curdir, "..", "scripts"))
 
 for (root, _, files) in walkdir(scripts_dir), file in files
+    # we don't want to test scripts that plot stuff, as that will break on
+    # travis.
+    splitdir(file)[2] in ("A-ensembles.jl",
+                          "A-ensembles-2.jl",
+                          "EX-crabs-xgb.jl",
+                          "EX-wine.jl") && continue
+
     @testset "testing $file" begin
         @test foo(joinpath(root, file))
     end
