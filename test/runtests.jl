77077e6fd4dfbe844b9aa144ec71c1088ec0e05b
Thibaut Lienart <ltib@me.com>
2019-11-11 10:17:12 +0100

    jd-update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,4 @@
-using Test
-
-foo(path) = (include(path); true)
+using Test, Logging
 
 const curdir = @__DIR__
 const scripts_dir = normpath(joinpath(curdir, "..", "scripts"))
@@ -15,6 +13,23 @@ for (root, _, files) in walkdir(scripts_dir), file in files
                           "ISL-lab-4.jl") && continue
 
     @testset "testing $file" begin
-        @test foo(joinpath(root, file))
+        path = joinpath(root, file)
+        tf = tempname()
+        write(tf, """
+        module Tester
+            include("$path")
+        end
+        """)
+        @test begin
+            bk = stdout
+            (rd, wr) = redirect_stdout()
+            Logging.disable_logging(Logging.LogLevel(3_000))
+            include(tf)
+            Logging.disable_logging(Logging.Debug)
+            redirect_stdout(bk)
+            close(wr)
+            close(rd)
+            true
+        end
     end
 end
