127ddc0a2523c352c0df1e124831bbb9201b3f98
Thibaut Lienart <ltib@me.com>
2019-11-18 18:09:06 +0100

    Updates

--- scripts/ISL-lab-9.jl
+++ scripts/ISL-lab-9.jl
@@ -7,5 +7,34 @@
 # using Pkg; Pkg.activate("."); Pkg.instantiate()
 # ```
 
+using MLJ, RDatasets, PrettyPrinting, Random
+
+Random.seed!(3203)
+X = randn(20, 2)
+y = vcat(-ones(10), ones(10))
+
+X = MLJ.table(X)
+y = categorical(y);
+
+@load SVC pkg=LIBSVM
+
+svc_mdl = SVC()
+svc = machine(svc_mdl, X, y)
+
+fit!(svc);
+
+ypred = predict(svc, X)
+misclassification_rate(ypred, y)
+
+rc = range(svc_mdl, :cost, lower=0.1, upper=5)
+tm = TunedModel(model=svc_mdl, ranges=[rc], tuning=Grid(resolution=10),
+                resampling=CV(nfolds=3, rng=33), measure=misclassification_rate)
+mtm = machine(tm, X, y)
+
+fit!(mtm)
+
+ypred = predict(mtm, X)
+misclassification_rate(ypred, y)
+
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
