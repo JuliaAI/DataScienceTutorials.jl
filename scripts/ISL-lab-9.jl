27ae31e6fa2e0a792eca93a12257eb675e892f58
Thibaut Lienart <ltib@me.com>
2019-11-18 16:33:43 +0100

    Updates

---
+++ scripts/ISL-lab-9.jl
@@ -0,0 +1,11 @@
+# Before running this, please make sure to activate and instantiate the environment
+# corresponding to [this `Project.toml`](https://raw.githubusercontent.com/alan-turing-institute/MLJTutorials/master/Project.toml) and [this `Manifest.toml`](https://raw.githubusercontent.com/alan-turing-institute/MLJTutorials/master/Manifest.toml)
+# so that you get an environment which matches the one used to generate the tutorials:
+#
+# ```julia
+# cd("MLJTutorials") # cd to folder with the *.toml
+# using Pkg; Pkg.activate("."); Pkg.instantiate()
+# ```
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
