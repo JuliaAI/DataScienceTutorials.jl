9c86fe1d08784238b4f1ed206c7b91097cd23e9b
Thibaut Lienart <ltib@me.com>
2020-02-19 23:48:16 +0100

    fd-update

--- _libs/lunr/lunrclient.js
+++ _libs/lunr/lunrclient.js
@@ -29,7 +29,7 @@ function parseLunrResults(results) {
         var item = PREVIEW_LOOKUP[id]
         var title = item["t"];
         var preview = item["p"];
-        var link = item["l"];
+        var link = item["l"].replace("__site/", "");
         var result = ('<li><span class="result-title"><a href="' + link + '">'
                     + title + '</a></span>');
         html.push(result);
