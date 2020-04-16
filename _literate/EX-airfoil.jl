060dd6ecaa8194e817c2d863a90c1c6529e579fc
Thibaut Lienart <ltib@me.com>
2020-04-16 18:48:44 +0200

    few fixes

--- _literate/EX-airfoil.jl
+++ _literate/EX-airfoil.jl
@@ -1,3 +1,5 @@
+# **Main author**: [Ashrya Agrawal](https://github.com/ashryaagr).
+#
 # ## Getting started
 # Here we use the [UCI "Airfoil Self-Noise" dataset](http://archive.ics.uci.edu/ml/datasets/Airfoil+Self-Noise)
 # ### Loading and  preparing the data
@@ -9,13 +11,14 @@ MLJ.color_off() # hide
 
 req = HTTP.get("https://raw.githubusercontent.com/rupakc/UCI-Data-Analysis/master/Airfoil%20Dataset/airfoil_self_noise.dat");
 
-df = CSV.read(req.body, header=[
-                  "Frequency","Attack_Angle","Chord+Length","Free_Velocity","Suction_Side","Scaled_Sound"
+df = CSV.read(req.body; header=[
+                  "Frequency","Attack_Angle","Chord+Length",
+                  "Free_Velocity","Suction_Side","Scaled_Sound"
               ]
        );
 df[1:5, :] |> pretty
 
-# 
+#
 
 schema(df)
 
@@ -37,6 +40,14 @@ for model in models(matching(X, y))
        print("Model Name: " , model.name , " , Package: " , model.package_name , "\n")
 end
 
+# Note that if we coerce `X.Frequency` to `Continuous`, many more models are available:
+
+coerce!(X, :Frequency=>Continuous)
+
+for model in models(matching(X, y))
+       print("Model Name: " , model.name , " , Package: " , model.package_name , "\n")
+end
+
 # ## DecisionTreeRegressor
 #
 # We will first try out DecisionTreeRegressor. We load the DecisionTreeRegressor from DecisionTree.jl
@@ -48,7 +59,7 @@ dcrm = machine(dcr, X, y)
 fit!(dcrm, rows=train, force=true)
 pred_dcrm = MLJ.predict(dcrm, rows=test);
 
-# Now you can call a loss function to asses the performace on test set.
+# Now you can call a loss function to assess the performance on test set.
 
 rms(pred_dcrm, y[test])
 
@@ -69,14 +80,12 @@ fit!(rfr_m, rows=train);
 pred_rfr = MLJ.predict(rfr_m, rows=test);
 rms(pred_rfr, y[test])
 
-# Clearly, we have improved the performace by using RandomForestRegressor
+# Unsurprisingly, the RandomForestRegressor does a better job.
 #
-# Can we do even better ?
-#
-# Yeah, we can!! We can make use of Model Tuning.
+# Can we do even better? Yeah, we can!! We can make use of Model Tuning.
 #
 # ## Tuning
-# 
+#
 # In case you are new to model tuning using MLJ, refer [lab5](https://alan-turing-institute.github.io/MLJTutorials/isl/lab-5/) and [model-tuning](https://alan-turing-institute.github.io/MLJTutorials/getting-started/model-tuning/)
 #
 # Range of values for parameters should be specified to do hyperparameter tuning
@@ -86,6 +95,7 @@ r_samF = range(rfr, :sampling_fraction, lower=0.6, upper=0.8)
 r = [r_maxD, r_samF];
 
 # Now we specify how the tuning should be done. Let's just specify a coarse grid tuning with cross validation and instantiate a tuned model:
+
 tuning = Grid(resolution=7)
 resampling = CV(nfolds=6)
 
