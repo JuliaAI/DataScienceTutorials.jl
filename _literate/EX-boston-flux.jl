5e704e962f86839abbc84e318f0e3453fa5a47fc
ayush-1506 <ayush.shridhar1506@gmail.com>
2020-06-12 14:52:54 +1000

    newer changes

--- _literate/EX-boston-flux.jl
+++ _literate/EX-boston-flux.jl
@@ -2,7 +2,13 @@
 #
 # ## Getting started
 
-using MLJFlux, MLJ, DataFrames, Statistics, Flux
+import MLJFlux
+import MLJ
+import DataFrames
+import Statistics
+import Flux
+using Random
+using Plots
 
 MLJ.color_off() # hide
 Random.seed!(11)
@@ -11,79 +17,78 @@ Random.seed!(11)
 # neural network regressor to predict the price of a house, 
 # given a number of features.
 
-features, targets = @load_boston
-features = DataFrame(features)
+features, targets = MLJ.@load_boston
+features = DataFrames.DataFrame(features)
 @show size(features)
 @show targets[1:3]
-first(features, 3) |> pretty
+first(features, 3) |> MLJ.pretty
 
 # Next obvious steps: partitioning into train and test set
 
-train, test = partition(eachindex(targets), 0.70, shuffle=true, rng=52)
+train, test = MLJ.partition(MLJ.eachindex(targets), 0.70, rng=52)
 
 # Let us try to implement an Neural Network regressor using
 # Flux.jl. MLJFlux.jl provides an MLJ interface to the Flux.jl
 # deep learning framework. The package provides four essential
-# models: `NeuralNetworkRegressor, MultivariateNeuralNetworkRegressor,
+# models: `NeuralNetworkRegressor, MultitargetNeuralNetworkRegressor,
 # NeuralNetworkClassifier` and `ImageClassifier`. 
 
 # At the heart of these models is a neural network. This is specified using
 # the `builder` parameter. Creating a builder object consists of two steps:
 # 1. Creating a new struct inherited from `MLJFlux.Builder`. `MLJFlux.Builder`
 # is an abstract structure used for the purpose of dispatching. Suppose we define
-# a new struct called `MyNNRegressor`. This can contain any attribute required to
+# a new struct called `MyNetworkBuilder`. This can contain any attribute required to
 # build the model later. (Step 2). Let's use Dense Neural Network with 2 hidden layers.
 
-mutable struct MyNNRegressor <: MLJFlux.Builder
+mutable struct MyNetworkBuilder <: MLJFlux.Builder
     n1::Int #Number of cells in the first hidden layer
     n2::Int #Number of cells in the second hidden layer
 end
 
 # Step 2: Building the neural network from this object.
-# Extend the `MLJFlux.fit` function. This takes in 3 arguments: The object of
-# `MyNNRegressor`, input dimension (ip) and output dimension (op).
+# Extend the `MLJFlux.build` function. This takes in 3 arguments: The object of
+# `MyNetworkBuilder`, input dimension (ip) and output dimension (op).
 
-function MLJFlux.fit(model::MyNNRegressor, ip, op)
-    return Flux.Chain(Flux.Dense(ip, model.n1), Flux.Dense(model.n1, model.n2), Flux.Dense(model.n2, op))
+function MLJFlux.build(model::MyNetworkBuilder, input_dims, output_dims)
+    layer1 = Flux.Dense(input_dims, model.n1)
+    layer2 = Flux.Dense(model.n1, model.n2)
+    layer3 = Flux.Dense(model.n2, output_dims)
+    return Flux.Chain(layer1, layer2, layer3)
 end
 
-# With all definitions ready, let us create an oject of this:
+# With all definitions ready, let us create an object of this:
 
-myregressor = MyNNRegressor(20, 10)
+myregressor = MyNetworkBuilder(20, 10)
 
 # Since the boston dataset is a regression problem, we'll be using
 # `NeuralNetworkRegressor` here. One thing to remember is that 
 # a `NeuralNetworkRegressor` object works seamlessly like any other
-# MLJ model: you can wrap it in an  MLJ `machine` and to anything
+# MLJ model: you can wrap it in an  MLJ `machine` and do anything
 # you'd do otherwise.
 
 # Let's start by defining our NeuralNetworkRegressor object, that takes `myregressor`
 # as it's parameter.
 
-nnregressor = NeuralNetworkRegressor(builder=myregressor, epochs=10)
+nnregressor = MLJFlux.NeuralNetworkRegressor(builder=myregressor, epochs=10)
 
-# Other parameters that `NeuralNetworkRegressor` takes in are:
-# optimiser = ADAM(0.001, (0.9, 0.999), IdDict{Any,Any}())
-# loss = Flux.mse,
-# epochs = 10,
-# batch_size = 1,
-# lambda = 0.0,
-# alpha = 0.0,
-# optimiser_changes_trigger_retraining = false
+# Other parameters that NeuralNetworkRegressor takes can be found here: 
+# https://github.com/alan-turing-institute/MLJFlux.jl#model-hyperparameters
 
 # `nnregressor` now acts like any other MLJ model. Let's try wrapping it in a 
-# MLJ machine and calling `fit!, predict!`.
+# MLJ machine and calling `fit!, predict`.
 
-mach = machine(nnregressor, features, targets)
+mach = MLJ.machine(nnregressor, features, targets)
 
 # Let's fit this on the train set
 
-fit!(mach, 3, rows=train, vebosity=3)
+MLJ.fit!(mach, rows=train, verbosity=3)
 
-# As we can see, the loss decreases at each epoch, showing the the neural network 
+# As we can see, the training loss decreases at each epoch, showing the the neural network 
 # is gradually learning form the training set.
 
-predict(mach, rows=test)
+preds = MLJ.predict(mach, features[test, :])
+
+print(preds[1:5])
 
 # Now let's retrain our model. One thing to remember is that retrainig may OR may not 
 # re-initialize our neural network model parameters. For example, changing the number of 
@@ -92,37 +97,50 @@ predict(mach, rows=test)
 
 nnregressor.epochs = 15
 
-fit!(mach, rows=train, verbosity=3)
+MLJ.fit!(mach, rows=train, verbosity=3)
 
 # You can always specify that you want to retrain the model from scratch using the force=true
 # parameter. (Look at documentation for `fit!` for more).
 
 # However, changing parameters such as batch_size will necessarily cause re-training from scratch.
 nnregressor.batch_size = 2
-fit!(mach, rows=train, verbosity=3)
+MLJ.fit!(mach, rows=train, verbosity=3)
 
 # Another bit to remember here is that changing the optimiser doesn't cause retaining by default.
 # However, the `optimiser_changes_trigger_retraining` in NeuralNetworkRegressor can be toggled to
-# accomodate this.
+# accomodate this. This allows one to modify the learning rate, for example, after an initial burn-in period.
 
+## Inspecting out-of-sample loss as a function of epochs
+
+r = MLJ.range(nnregressor, :epochs, lower=1, upper=30, scale=:log10)
+curve = MLJ.learning_curve(nnregressor, features, targets,
+                       range=r,
+                       resampling=MLJ.Holdout(fraction_train=0.7),
+                       measure=MLJ.rms)
+
+plot(curve.parameter_values,
+    curve.measurements,
+    xlab=curve.parameter_name,
+    xscale=curve.parameter_scale,
+    ylab = "RMS")
 ## Tuning
 
 # As mentioned above, `nnregressor` can act like any other MLJ model. Let's try to tune the
 # batch_size parameter.
 
-bs = range(nnregressor, :batch_size, lower=1, upper=5)
+bs = MLJ.range(nnregressor, :batch_size, lower=1, upper=5)
 
-tm = TunedModel(model=nnregressor, ranges=[bs, ], measure=Flux.mse)
+tm = MLJ.TunedModel(model=nnregressor, ranges=[bs, ], measure=MLJ.l2)
 
 # For more on tuning, refer to the model-tuning tutorial.
 
-m = machine(tm, features, targets)
+m = MLJ.machine(tm, features, targets)
 
-fit!(m)
+MLJ.fit!(m)
 
 # This evaluated the model at each value of our range.
 # The best value is:
 
-fitted_params(m).best_model.batch_size
+MLJ.fitted_params(m).best_model.batch_size
 
 
