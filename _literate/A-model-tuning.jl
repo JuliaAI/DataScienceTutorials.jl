66aff90d5ff0b11c2b1ff9de75552dd2c6173efb
gdfix <geoffroy.dolphin@gmail.com>
2020-06-22 17:44:54 +0200

    Update A-model-tuning.jl

--- _literate/A-model-tuning.jl
+++ _literate/A-model-tuning.jl
@@ -31,10 +31,10 @@ r   = range(dtc, :max_depth, lower=1, upper=5)
 
 tm = TunedModel(model=dtc, ranges=[r, ], measure=cross_entropy)
 
-# Note that "wrapping a model in a tuning strategy" as above means creating a new "self-tuning" version of the model, tuned_model = TunedModel(model=...), in which further key-word arguments specify:
-# 1. the algorithm (a.k.a., tuning strategy) for searching the hyper-parameter space of the model (e.g., tuning=Random(rng=123)or tuning=Grid(goal=100)).
-# 2. the resampling strategy, used to evaluate performance for each value of the hyper-parameters (e.g., resampling=CV(nfolds=9, rng=123) or resampling=Holdout(fraction_train=0.7)).
-# 3. the measure (or measures) on which to base performance evaluations (and for reporting purposes) (e.g., measure = rms or measures=[rms, mae]).
+# Note that "wrapping a model in a tuning strategy" as above means creating a new "self-tuning" version of the model, `tuned_model = TunedModel(model=...)`, in which further key-word arguments specify:
+# 1. the algorithm (a.k.a., tuning strategy) for searching the hyper-parameter space of the model (e.g., `tuning=Random(rng=123)or tuning=Grid(goal=100)`).
+# 2. the resampling strategy, used to evaluate performance for each value of the hyper-parameters (e.g., `resampling=CV(nfolds=9, rng=123)` or `resampling=Holdout(fraction_train=0.7)`).
+# 3. the measure (or measures) on which to base performance evaluations (and for reporting purposes) (e.g., `measure = rms` or `measures=[rms, mae]`).
 # 4. the range, usually describing the "space" of hyperparameters to be searched (but more generally whatever extra information is required to complete the search specification, e.g., initial values in gradient-descent optimization).
 
 # For more options do `?TunedModel`.
