5b3282e8d64912f472de24e7dacc8926d5e8f657
gdfix <geoffroy.dolphin@gmail.com>
2020-07-10 15:38:53 +0200

    Syntax update (MLJ 0.12.0)

--- _literate/A-composing-models.jl
+++ _literate/A-composing-models.jl
@@ -13,18 +13,19 @@ using MLJ
 using PrettyPrinting
 
 @load KNNRegressor
-# input
+## input
 X = (age    = [23, 45, 34, 25, 67],
      gender = categorical(['m', 'm', 'f', 'm', 'f']))
-# target
+## target
 height = [178, 194, 165, 173, 168];
 
 # Note that the scientific type of `age` is `Count` here:
+
 scitype(X.age)
 
 # We will want to coerce that to `Continuous` so that it can be given to a regressor that expects such values.
 
-## ## Declaring a pipeline
+# ## Declaring a pipeline
 # A typical workflow for such data is to one-hot-encode the categorical data and then apply some regression model on the data.
 # Let's say that we want to apply the following steps:
 # 1. standardize the target variable (`:height`)
@@ -32,6 +33,7 @@ scitype(X.age)
 # 1. train a KNN regression model
 
 # The `@pipeline` macro helps you define such a simple (non-branching) pipeline of steps to be applied in order:
+
 pipe = @pipeline(X -> coerce(X, :age=>Continuous),
                 OneHotEncoder(),
                 KNNRegressor(K=3),
@@ -39,12 +41,13 @@ pipe = @pipeline(X -> coerce(X, :age=>Continuous),
 
 # Note the coercion of the `:age` variable to Continuous since `KNNRegressor` expects `Continuous` input.
 # Note also the `target` keyword where you can specify a transformation of the target variable.
+
 # Hyperparameters of this pipeline can be accessed (and set) using dot syntax:
+
 pipe.knn_regressor.K = 2
 pipe.one_hot_encoder.drop_last = true;
 
 # Evaluation for a pipe can be done with the `evaluate!` method; implicitly it will construct machines that will contain the fitted parameters etc:
+
 evaluate(pipe, X, height, resampling=Holdout(),
          measure=rms) |> pprint
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
