a7f10e51ac2d00457853f4e190298c0abe1f42ca
OkonSamuel <okonsamuel50@gmail.com>
2022-01-14 00:07:19 +0100

    avoid using deprecated @pipeline macro

--- _literate/A-composing-models/tutorial.jl
+++ _literate/A-composing-models/tutorial.jl
@@ -29,27 +29,36 @@ scitype(X.age)
 
 # A typical workflow for such data is to one-hot-encode the categorical data and then apply some regression model on the data.
 # Let's say that we want to apply the following steps:
-# 1. standardize the target variable (`:height`)
-# 1. one hot encode the categorical data
-# 1. train a KNN regression model
+# 1. One hot encode the categorical features in `X`
+# 1. Standardize the target variable (`:height`)
+# 1. Train a KNN regression model on the one hot encoded data and the Standardized target.
 
-# The `@pipeline` macro helps you define such a simple (non-branching) pipeline of steps to be applied in order:
+# The `Pipeline` constructor helps you define such a simple (non-branching) pipeline of steps to be applied in order:
 
-pipe = @pipeline(
-    X -> coerce(X, :age=>Continuous),
-    OneHotEncoder(),
-    KNNRegressor(K=3),
-    target = UnivariateStandardizer());
+pipe = Pipeline(
+    coercer = X -> coerce(X, :age=>Continuous),
+    one_hot_encoder = OneHotEncoder(),
+    transformed_target_model = TransformedTargetModel(
+        model = KNNRegressor(K=3);
+        target=UnivariateStandardizer()
+    )
+)
 
 # Note the coercion of the `:age` variable to Continuous since `KNNRegressor` expects `Continuous` input.
-# Note also the `target` keyword where you can specify a transformation of the target variable.
+# Note also the `TransformedTargetModel` which allows one to learn a transformation (in this case Standardization) of the 
+# target variable to be passed to the `KNNRegressor`.
 
 # Hyperparameters of this pipeline can be accessed (and set) using dot syntax:
 
-pipe.knn_regressor.K = 2
+pipe.transformed_target_model.model.K = 2
 pipe.one_hot_encoder.drop_last = true;
 
 # Evaluation for a pipe can be done with the `evaluate!` method; implicitly it will construct machines that will contain the fitted parameters etc:
 
-evaluate(pipe, X, height, resampling=Holdout(),
-         measure=rms) |> pprint
+evaluate(
+    pipe,
+    X,
+    height,
+    resampling=Holdout(),
+    measure=rms
+) |> pprint
