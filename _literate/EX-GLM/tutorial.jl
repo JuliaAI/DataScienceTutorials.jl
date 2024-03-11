5079b1f5cfee1bc7344b5ed12a6d2c9f1ebda8ab
Essam <essamwisam@outlook.com>
2024-03-11 17:53:03 +0200

    Update EX-GLM

--- _literate/EX-GLM/tutorial.jl
+++ _literate/EX-GLM/tutorial.jl
@@ -95,9 +95,7 @@ fp = fitted_params(LinearModel)
 yhatResponse = [ŷ[i,1].μ for i in 1:nrow(y)]
 residuals = y .- yhatResponse
 r = report(LinearModel)
-
-k = collect(keys(fp.fitted_params_given_machine))[3]
-println("\n Coefficients:  ", fp.fitted_params_given_machine[k].coef)
+println("\n Coefficients:  ", fp.linear_regressor.coef)
 println("\n y \n ", y[1:5,1])
 println("\n ŷ \n ", ŷ[1:5])
 println("\n yhatResponse \n ", yhatResponse[1:5])
@@ -147,8 +145,7 @@ fp = fitted_params(LogisticModel)
 residuals = [1 - pdf(ŷ[i], y[i,1]) for i in 1:nrow(y)]
 r = report(LogisticModel)
 
-k = collect(keys(fp.fitted_params_given_machine))[3]
-println("\n Coefficients:  ", fp.fitted_params_given_machine[k].coef)
+println("\n Coefficients:  ", fp.linear_binary_classifier.coef)
 println("\n y \n ", y[1:5,1])
 println("\n ŷ \n ", ŷ[1:5])
 println("\n residuals \n ", residuals[1:5])
