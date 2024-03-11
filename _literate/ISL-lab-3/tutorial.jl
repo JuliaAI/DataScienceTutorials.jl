786d5e66816dfac2f36175e1c430c3b142669bf0
Essam <essamwisam@outlook.com>
2024-03-11 22:42:52 +0200

    Update _literate/ISL-lab-3/tutorial.jl
    Co-authored-by: Anthony Blaom, PhD <anthony.blaom@gmail.com>

--- _literate/ISL-lab-3/tutorial.jl
+++ _literate/ISL-lab-3/tutorial.jl
@@ -99,8 +99,7 @@ round(rms(ŷ, y), sigdigits=4)
 
 res = ŷ .- y
 plot(res, line=:stem, linewidth=1, marker=:circle, legend=false, size=((800,600)))
-hline!([0], linewidth=2, color=:red)
-# add a horizontal line at x=0
+hline!([0], linewidth=2, color=:red)    # add a horizontal line at x=0
 savefig(joinpath(@OUTPUT, "ISL-lab-3-res.svg")) # hide
 
 # \figalt{Plot of the residuals}{ISL-lab-3-res.svg}
