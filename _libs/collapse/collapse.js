a3ef66a2464795a92e75c8ec21b1e38fc8b0e680
Essam <essamwisam@outlook.com>
2024-04-11 18:37:53 +0200

    fd-update

--- _libs/collapse/collapse.js
+++ _libs/collapse/collapse.js
@@ -1,3 +1,11 @@
+// This adds the collapsing sections feature to all tutorials and sidebar
+
+// first get info on whether hosted or not
+let isHosted = window.location.origin.includes("github.io");
+// accordingly get icons
+const rightIcon = (isHosted) ? origin + "/DataScienceTutorials.jl" + "/assets/right-icon.svg" : "/assets/right-icon.svg";
+const downIcon = (isHosted) ? origin + "/DataScienceTutorials.jl" + "/assets/left-icon.svg" : "/assets/down-icon.svg";
+
 document.addEventListener("DOMContentLoaded", function () {
   var filename = window.location.pathname;
   // Get all elements with class name "dropdown"
@@ -15,22 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
     triangleImage.src = dropdown.nextElementSibling.classList.contains(
       "collapse"
     )
-      ? "/assets/right-icon.svg"
-      : "/assets/down-icon.svg";
+      ? rightIcon
+      : downIcon;
 
-      // check local storage localStorage.setItem("dropdown-" + index, newContent); but get
-     const restoreMemory = function (localName) {
-     if (localStorage.getItem(localName)) {
-      const newContent = localStorage.getItem(localName);
-      triangleImage.src =  (newContent.includes("right-icon")) ? "/assets/right-icon.svg" : "/assets/down-icon.svg";
-      var dropdownContent = dropdown.nextElementSibling;
+    // check local storage localStorage.setItem("dropdown-" + index, newContent); but get
+    const restoreMemory = function (localName) {
+      if (localStorage.getItem(localName)) {
+        const newContent = localStorage.getItem(localName);
+        triangleImage.src = (newContent.includes("right-icon")) ? rightIcon : downIcon;
+        var dropdownContent = dropdown.nextElementSibling;
         if (newContent.includes("down-icon")) {
           dropdownContent.style.display = "block";
         }
-       else {
-         dropdownContent.style.display = "none";
-       }
-     }
+        else {
+          dropdownContent.style.display = "none";
+        }
+      }
     }
 
     restoreMemory("dropdown-" + index + filename);
@@ -69,23 +77,23 @@ document.addEventListener("DOMContentLoaded", function () {
         if (dropdownContent.classList.contains("dropdown-content")) {
           dropdownContent.style.display = "block";
         }
-        
+
       }
       triangleImage.src = newContent;
 
       // store this state using dropdown index
-      localStorage.setItem("dropdown-" + index + filename , newContent);
+      localStorage.setItem("dropdown-" + index + filename, newContent);
       // if the element is li store it in local storage
       if (heading.tagName === "LI") {
-        localStorage.setItem("dropdown-" + index + "li" , newContent);
+        localStorage.setItem("dropdown-" + index + "li", newContent);
       }
     }
 
     // if heading is an li we don't want to be restricted to clicking on the triangle
-    if (heading.tagName === "LI") {      
-    dropdown.addEventListener("click", activation);
-  }
-   // otherwise there is already an attached onclick event
+    if (heading.tagName === "LI") {
+      dropdown.addEventListener("click", activation);
+    }
+    // otherwise there is already an attached onclick event
     else {
       triangleImage.addEventListener("click", activation);
     }
